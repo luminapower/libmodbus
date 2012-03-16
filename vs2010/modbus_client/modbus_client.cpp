c290b9348f443b9cddb321cadc8b28d64edaadb3
Julian OBrien <jmobrien@luminapower.com>
2012-03-16 09:03:51 -0400

    Constants, right?

--- vs2010/modbus_client/modbus_client.cpp
+++ vs2010/modbus_client/modbus_client.cpp
@@ -10,21 +10,38 @@ using namespace std;
 
 #define MAXIMUM_NUMBER_OF_REGISTERS 123
 
+#define IP_ADDRESS "192.168.1.230"
+#define IP_PORT    502
+
+#define BUFFER_SIZE 16
+
 uint16_t register_values[MAXIMUM_NUMBER_OF_REGISTERS];
 
 unsigned int i;
-char buffer[16];
+char buffer[BUFFER_SIZE];
 
 unsigned int command;
 unsigned int starting_address;
 unsigned int number_of_registers;
+
+
+struct timeval response_timeout;
 
 modbus_t *ctx;
 int register_count;
 
 int init_modbus(){
 	// init modbus
-  ctx = modbus_new_tcp("192.168.1.230", 502);
+  ctx = modbus_new_tcp(IP_ADDRESS, IP_PORT);
+	modbus_set_debug(ctx, true);
+
+	response_timeout.tv_sec = 10;
+  response_timeout.tv_usec = 0;
+	modbus_set_response_timeout(ctx, &response_timeout);
+
+	modbus_get_response_timeout(ctx, &response_timeout);
+	printf("Timeout: %d.%ds\n", response_timeout.tv_sec, response_timeout.tv_usec);
+
 	if (modbus_connect(ctx) == -1) {
     fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
     modbus_free(ctx);
@@ -40,7 +57,7 @@ int process_write_command() {
 
 	// get the write values
 	for (i = 0; i < number_of_registers; i++) {
-		cin.getline(buffer, 16);
+		cin.getline(buffer, BUFFER_SIZE);
 		sscanf_s(buffer, "%u", &register_values[i]);
 		printf("reg[%d]=%u\n", starting_address + i, register_values[i]);
 	}
